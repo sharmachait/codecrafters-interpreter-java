132e247ea146fba099c73ca5cf2c8122f1d262b4
sharmachait <chait8126@gmail.com>
2025-08-10 18:32:32 +0530

    Scanning: Other single-character tokens #xc5

--- src/main/java/lexicon/TokenType.java
+++ src/main/java/lexicon/TokenType.java
@@ -1,5 +1,49 @@
 package lexicon;
 
 public enum TokenType {
-    LEFT_PAREN, RIGHT_PAREN, EOF, LEFT_BRACE, RIGHT_BRACE
+    // single character tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    COLON,
+    QUESTION,
+    STAR,
+
+    // Maybe single character token
+    SLASH,
+    BANG,
+    EQUALITY,
+    GREATER,
+    LESS,
+
+    // multi character tokens
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    AND,
+    CLASS,
+    ELSE,
+    IF,
+    FALSE,
+    TRUE,
+    FUN,
+    FOR,
+    WHILE,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    VAR,
+    BREAK,
+    CONTINUE,
+
+    EOF
 }
